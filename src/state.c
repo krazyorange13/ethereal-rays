febb2c49e164b00c2064fffa1ad3cdbddf4ca834
Krazy0range <77130315+krazyorange13@users.noreply.github.com>
2024-12-16 20:09:09 -0600

    begin entities
    is it just me or is writing boilerplate super satisfying? especially when u got a good theme and font and that smooth cursor go brrr

--- src/state.c
+++ src/state.c
@@ -3,10 +3,53 @@
 
 ETHER_state_keybinds create_default_state_keybinds()
 {
-    ETHER_state_keybinds s;
-    s.move_up = STATE_DEFAULT_KEYBINDS_MOVE_UP;
-    s.move_down = STATE_DEFAULT_KEYBINDS_MOVE_DOWN;
-    s.move_left = STATE_DEFAULT_KEYBINDS_MOVE_LEFT;
-    s.move_right = STATE_DEFAULT_KEYBINDS_MOVE_RIGHT;
-    return s;
+    ETHER_state_keybinds state;
+    state.move_up = STATE_DEFAULT_KEYBINDS_MOVE_UP;
+    state.move_down = STATE_DEFAULT_KEYBINDS_MOVE_DOWN;
+    state.move_left = STATE_DEFAULT_KEYBINDS_MOVE_LEFT;
+    state.move_right = STATE_DEFAULT_KEYBINDS_MOVE_RIGHT;
+    return state;
+}
+
+ETHER_state_entities ETHER_create_state_entities()
+{
+    ETHER_state_entities entities;
+    entities.count = 0;
+    entities.head = NULL;
+    entities.tail = NULL;
+    return entities;
+}
+
+void ETHER_state_entities_add(ETHER_state_entities *entities, ETHER_entity *entity)
+{
+    if (entities->count)
+    {
+        entities->tail->next = entity;
+        entity->prev = entities->tail;
+        entities->tail = entity;
+        entity->next = NULL;
+    }
+    else
+    {
+        entities->head = entity;
+        entities->tail = entity;
+        entity->next = NULL;
+        entity->prev = NULL;
+    }
+    entities->count++;
+}
+
+ETHER_entity *ETHER_state_entities_pop(ETHER_state_entities *entities)
+{
+    if (entities->count)
+    {
+        ETHER_entity *p = entities->tail;
+        p->prev->next = NULL;
+        p->prev = NULL;
+        return p;
+    }
+    else
+    {
+        return NULL;
+    }
 }
--- src/state.h
+++ src/state.h
@@ -8,6 +8,9 @@
 typedef struct _ETHER_state ETHER_state;
 typedef struct _ETHER_state_input ETHER_state_input;
 typedef struct _ETHER_state_keybinds ETHER_state_keybinds;
+typedef struct _ETHER_state_entities ETHER_state_entities;
+
+typedef struct _ETHER_entity ETHER_entity;
 
 ETHER_state_keybinds create_default_state_keybinds();
 
@@ -31,6 +34,26 @@ struct _ETHER_state_keybinds
     SDL_Scancode move_down;
     SDL_Scancode move_right;
     SDL_Scancode move_left;
+};
+
+struct _ETHER_state_entities
+{
+    uint16_t count;
+    ETHER_entity *head;
+    ETHER_entity *tail;
+};
+
+/* entities */
+
+void ETHER_state_entities_add(ETHER_state_entities *entities, ETHER_entity *entity);
+ETHER_entity *ETHER_state_entities_pop(ETHER_state_entities *entities);
+
+struct _ETHER_entity
+{
+    ETHER_vec2 pos;
+    ETHER_col col;
+    struct _ETHER_entity *next;
+    struct _ETHER_entity *prev;
 };
 
 #endif
